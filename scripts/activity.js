908e61f753ab7e06c70f467862eb316bc96b325b
boonakij <boonakij@gmail.com>
2019-06-04 16:37:03 -0500

    functionality progress

--- scripts/activity.js
+++ scripts/activity.js
@@ -1,7 +1,61 @@
 $( document ).ready(function() {
 
-  $(".clear-button").click(function() {
+  var maxClips = 5;
+  var currentClip = 1;
+
+  function clearAnswers() {
     $("input").prop('checked', false);
+  }
+
+  function allAnswered() {
+    var allAnswered = true;
+    $("form").each(function() {
+        console.log($(this).serialize());
+        if ($(this).serialize() == "") {
+          allAnswered = false;
+        }
+    });
+    return allAnswered;
+  }
+
+  $(".clear-button").click(function() {
+    clearAnswers();
   });
+
+  $(".next-button").click(function() {
+    if (currentClip < maxClips) {
+      currentClip += 1;
+      $(".previous-clip-number").html(currentClip - 1);
+      $(".clip-number").html(currentClip);
+      $(".next-clip-number").html(currentClip + 1);
+      $(".back-button").show();
+      clearAnswers();
+      $(".next-button").hide();
+    }
+  });
+
+  $(".back-button").click(function() {
+    if (currentClip > 1) {
+      currentClip -= 1;
+      $(".previous-clip-number").html(currentClip - 1);
+      $(".clip-number").html(currentClip);
+      $(".next-clip-number").html(currentClip + 1);
+      if (currentClip == 1) {
+        $(".back-button").hide();
+      }
+    }
+  });
+
+  $('input').on('change', function() {
+    if (allAnswered()) {
+      if (currentClip < maxClips) {
+        $(".next-button").show();
+      }
+      else {
+        $(".submit-button").show();
+      }
+    }
+  });
+
 
 });
